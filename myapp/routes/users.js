d733d14a9d685b5d0e5eab7edae10103e7799b3c
Agustina Arduino <agustinaarduino@Agustinas-MacBook-Air.local>
2024-11-13 12:17:17 -0300

    controller y rutas de user

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+const userController = require('../controllers/userController');
+
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+});*/
+
+router.get('/register', userController.register);
+
+router.get('/login', userController.login);
 
 module.exports = router;
