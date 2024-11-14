fce1c8e91fc42093120600d76606dd48a6f4dae6
Agustina Arduino <agustinaarduino@Agustinas-MacBook-Air.local>
2024-11-14 14:19:44 -0300

    update de userController y base de datos

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 const userController = require('../controllers/userController');
 
+/* GET users listing. */
 /*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });*/
 
 router.get('/register', userController.register);
+router.post('/register', userController.results);
 
 router.get('/login', userController.login);
+router.post('/login', userController.loginUser)
+
+
+router.post("/logout", userController.logout);
 
 module.exports = router;
