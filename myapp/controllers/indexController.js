0fb0279bb0fd344dbd20bfee490b6e5cf814370b
Agustina Arduino <agustinaarduino@Agustinas-MacBook-Air.local>
2024-11-14 16:19:24 -0300

    cambios sofi

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -4,18 +4,32 @@ const producto = db.Producto;
 
 const indexController = {
     index: (req, res) => {
-        res.render('index')
+        producto.findAll()
+            .then((producto) => {
+                res.render('index', { producto: producto });
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     },
     detalle: (req, res) => {
         let productoEncontradoId = req.params.id;
 
         producto.findByPk(productoEncontradoId)
           .then((producto) => {
-            return res.render('productAdd', {producto: producto})
+            return res.render('product', {producto: producto})
           })
           .catch(function(error) {
             return console.log(error);
            })
+    },
+    img: (req, res) => {
+      let productoEncontradoId = req.params.id;
+
+      if (productoEncontradoId == 1) {
+        let imagenProducto = "img-tv-samsung-smart.jpg";
+        res.render("index", {imagen: imagenProducto})
+      }
     }
 };
 
